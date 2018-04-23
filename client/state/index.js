aed9be120e6946360a869a3ddb59bf6212b63e71
spen <spen_@hotmail.co.uk>
2018-04-23 17:37:37 +0000

    SiteAddressChanger: Rename state slice

--- client/state/index.js
+++ client/state/index.js
@@ -81,7 +81,7 @@ import signup from './signup/reducer';
 import simplePayments from './simple-payments/reducer';
 import sites from './sites/reducer';
 import siteRoles from './site-roles/reducer';
-import siteRename from './site-rename/reducer';
+import siteAddressChange from './site-rename/reducer';
 import siteSettings from './site-settings/reducer';
 import stats from './stats/reducer';
 import storedCards from './stored-cards/reducer';
@@ -170,7 +170,7 @@ const reducers = {
 	signup,
 	sites,
 	siteRoles,
-	siteRename,
+	siteAddressChange,
 	siteSettings,
 	simplePayments,
 	stats,
