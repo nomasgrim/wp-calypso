d764a214b0c1c05e9609bc346172462d31f7397c
Jon Surrell <jon.surrell@automattic.com>
2018-05-04 16:25:26 +0200

    Use non-site props where available

--- client/me/purchases/manage-purchase/index.jsx
+++ client/me/purchases/manage-purchase/index.jsx
@@ -353,9 +353,12 @@ class ManagePurchase extends Component {
 		return (
 			<div className="manage-purchase__content">
 				<span className="manage-purchase__description">{ description }</span>
-				<span className="manage-purchase__settings-link">
-					<ProductLink selectedPurchase={ purchase } selectedSite={ selectedSite } />
-				</span>
+				{ /* Disconnected sites can't link to their product pages */
+				selectedSite && (
+					<span className="manage-purchase__settings-link">
+						<ProductLink selectedPurchase={ purchase } selectedSite={ selectedSite } />
+					</span>
+				) }
 			</div>
 		);
 	}
